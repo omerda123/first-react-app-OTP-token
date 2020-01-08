6baf5dbb0afdaaf0a86d9539ae74dcd02fe5d2a2
Omer Daniel <omerda@gmail.com>
2020-01-08 16:46:18 +0200

    add build

---
+++ build/precache-manifest.29aa5b993dd4a14f687ce6c4e88748be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69a530d9c55ea45d17a32b2f5a011a6",
+    "url": "/omerda123/first-react-app-OTP-token/index.html"
+  },
+  {
+    "revision": "876411a6a54e817fde58",
+    "url": "/omerda123/first-react-app-OTP-token/static/css/main.b70b4942.chunk.css"
+  },
+  {
+    "revision": "aadbdc4987c1d1267c3d",
+    "url": "/omerda123/first-react-app-OTP-token/static/js/2.16ed718d.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/omerda123/first-react-app-OTP-token/static/js/2.16ed718d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "876411a6a54e817fde58",
+    "url": "/omerda123/first-react-app-OTP-token/static/js/main.f3ba574b.chunk.js"
+  },
+  {
+    "revision": "f9f036d4caf28b2fcc18",
+    "url": "/omerda123/first-react-app-OTP-token/static/js/runtime-main.42f493fd.js"
+  }
+]);
